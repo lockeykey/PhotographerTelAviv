a136d436a24cf650aaaa6d41e9db6d61e759b9bc
lockekey <elena.varrava@gmail.com>
2017-12-19 22:15:11 +0200

    some changes in gallery render file

--- js/portfolioGalleryRender.js
+++ js/portfolioGalleryRender.js
@@ -3,37 +3,37 @@ new Vue({
   
   data: { 
       cards: [
-          {imageSource: '../images/imagesForMainPage/hry1of5.jpg',
+          {imageSource: './images/hry1of5.jpg',
            imageAlt: 'hrystya1',
            storyDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
            storyHeader: 'Денис и Света',
            StoryLink: '#'
           },
-          {imageSource: '../images/imagesForMainPage/hry2of5.jpg',
+          {imageSource: './images/hry2of5.jpg',
            imageAlt: 'hrystya2',
            storyDescription: 'апмLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
            storyHeader: 'Ваcя и Света',
            StoryLink: '#'
           },
-          {imageSource: '../images/imagesForMainPage/hry3of5.jpg',
+          {imageSource: './images/hry3of5.jpg',
            imageAlt: 'hrystya3',
            storyDescription: 'аваLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
            storyHeader: 'Денис и Денис',
            storyLink: '#'
           },
-          {imageSource: '../images/imagesForMainPage/hry4of5.jpg',
+          {imageSource: './images/hry4of5.jpg',
            imageAlt: 'hrystya4',
            storyDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
            storyHeader: 'Женя и Света',
            StoryLink: '#'
           },
-          {imageSource: '../images/imagesForMainPage/hry5of5.jpg',
+          {imageSource: './images/hry5of5.jpg',
            imageAlt: 'hrystya5',
            storyDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
            storyHeader: 'Ваcя и Жираф',
            StoryLink: '#'
           },
-          {imageSource: '../images/imagesForMainPage/hry1of5.jpg',
+          {imageSource: './images/hry1of5.jpg',
            imageAlt: 'hrystya1',
            storyDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
            storyHeader: 'Ваcя и Света',
