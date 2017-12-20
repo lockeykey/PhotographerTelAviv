eda222e13ea0285e7d5e255f7983be826124b25f
lockekey <elena.varrava@gmail.com>
2017-12-20 13:40:31 +0200

    corrected maistake in address

--- js/portfolioGalleryRender.js
+++ js/portfolioGalleryRender.js
@@ -6,41 +6,39 @@ new Vue({
           {imageSource: './images/hry1of5.jpg',
            imageAlt: 'hrystya1',
            storyDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-           storyHeader: 'Денис и Света',
+           storyHeader: 'Lorem ipsum',
            StoryLink: '#'
           },
           {imageSource: './images/hry2of5.jpg',
            imageAlt: 'hrystya2',
            storyDescription: 'апмLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-           storyHeader: 'Ваcя и Света',
+           storyHeader: 'Incididunt ut labore',
            StoryLink: '#'
           },
           {imageSource: './images/hry3of5.jpg',
            imageAlt: 'hrystya3',
-           storyDescription: 'аваLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-           storyHeader: 'Денис и Денис',
+           storyDescription: 'dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+           storyHeader: 'Labore et dolore',
            storyLink: '#'
           },
           {imageSource: './images/hry4of5.jpg',
            imageAlt: 'hrystya4',
            storyDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-           storyHeader: 'Женя и Света',
+           storyHeader: 'Adipiscing elit',
            StoryLink: '#'
           },
           {imageSource: './images/hry5of5.jpg',
            imageAlt: 'hrystya5',
            storyDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-           storyHeader: 'Ваcя и Жираф',
+           storyHeader: 'Tempor incididunt',
            StoryLink: '#'
           },
           {imageSource: './images/hry1of5.jpg',
            imageAlt: 'hrystya1',
            storyDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-           storyHeader: 'Ваcя и Света',
+           storyHeader: 'Eiusmod tempor',
            StoryLink: '#'
           },
          
          ]}
         });
-         
-      
